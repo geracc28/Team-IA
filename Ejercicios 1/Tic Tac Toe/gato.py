dc6ded7e4003b500f1026b74e7e76d6c007ebb39
Robin-OS <106449406+Robin-OS@users.noreply.github.com>
2024-03-03 18:14:28 -0600

    V1.1 gato.py

--- Ejercicios 1/Tic Tac Toe/gato.py
+++ Ejercicios 1/Tic Tac Toe/gato.py
@@ -3,6 +3,9 @@
 import tkinter as tk
 from tkinter import *
 
+# Se declara el diccionario que servira para colocar los valores en el tablero como casilla ocupada o no, se inicializa en 0s
+casillas = dict(a0="0", a1="0", a2="0", a3="0", a4="0", a5="0", a6="0", a7="0", a8="0")
+
 # Esta funcion se encarga de comenzar el tablero en blanco
 def dibujaTablero():
     ventana = tk.Tk()
@@ -18,13 +21,26 @@ def dibujaTablero():
     canvas.create_line(0,400,600,400, width=5, fill='gold')
     # Linea separadora
     canvas.create_line(600,0,600,600, fill="gainsboro")
+    # Funcion para poder terminar la ejecucion, se coloca de forma anidada para no tener que pasar por referencia "ventana" 
+    def salirJuego():
+        ventana.destroy()
     # Se declaran los botones de salir y jugar
-    botonSalir = tk.Button(ventana, text="Salir", bg="khaki", fg="black",font=("Courier New", 25, "bold"), width=8, height=3)
+    botonSalir = tk.Button(ventana, text="Salir", command=salirJuego, bg="khaki", fg="black",font=("Courier New", 25, "bold"), width=8, height=3)
     botonSalir.place(x=625, y=300)
-    botonJugar = tk.Button(ventana, text="Jugar", bg="sandybrown", fg="black", font=("Courier New", 25, "bold"), width=8, height=3)
+    botonJugar = tk.Button(ventana, text="Jugar", command=limpiaTablero,  bg="sandybrown", fg="black", font=("Courier New", 25, "bold"), width=8, height=3)
     botonJugar.place(x=625, y=100)
     # Mantiene la ventana abierta
     ventana.mainloop()
+
+# Esta funcion se utiliza para inicializar el juego, colocar todo en 0s, se debe llamar por el boton de jugar
+def limpiaTablero():
+    print("Los valores de las casillas quedaron: ")
+    print(casillas)
+    # Se itera sobre el diccionario en claves y valores para colocar todo en un solo valor
+    for clave, valor in casillas.items():
+        casillas[clave] = "1"
+    print("Los valores de las casillas son: ")
+    print(casillas)
 
 
 # Funcion principal
