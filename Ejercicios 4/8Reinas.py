a914558ed370e8706d4b750db9b6448745c2673e
geracc28 <gerardoonasis@gmail.com>
2024-04-13 14:03:55 -0600

    Inicialización de tablero

---
+++ Ejercicios 4/8Reinas.py
@@ -0,0 +1,19 @@
+
+if __name__ == "__main__":
+    print("Juego de las 8-reinas")
+    #Inicializa el tablero con 0 reinas
+    tablero = [
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+    # Comienza a colocar reinas
+    
+
+def revisaAtaques(matriz):
+    print("---")
